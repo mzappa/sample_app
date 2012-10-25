af5125e440e5c1337acafb52e4dd3ce9e77fada4
Mario Zappa <mzappa@comcast.net>
2012-10-25 18:06:41 -0500

    Initial checkin of the new but empty Piss On Demand project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pod::Application.config.secret_token = 'f892d88caa64e93486e2dea355d96a763864f3da0fa1bd3964231f7143d024ae66b883168fdd92619b413206141bea71edb33e616a4e5c294e93f307d2de9144'
